f7b8072a669edf96b734278035db41ce35517c4f
Pedro Mariano <plmariano@fc.ul.pt>
2014-01-20 00:58:18 +0000

    First implementation of a reactive bee model.
    Bees have a vector of behaviour rules stored from highest priority to lowest priority.  Each rule has a pre
    condition and an action.  The pre condition is stimulus range.  The range must have at least one limit.  The
    action is either to move up the stimulus gradient or move down.

--- robots/Bee.cpp
+++ robots/Bee.cpp
@@ -7,7 +7,7 @@
 #include <boost/foreach.hpp>
 #include <boost/math/constants/constants.hpp>
 
-#include <PhysicalEngine.h>
+#include <enki/PhysicalEngine.h>
 
 #include "Bee.h"
 
@@ -22,13 +22,21 @@ int sign(T val)
 
 namespace Enki
 {
-	double Bee::LENGTH = 2;
+	const double Bee::LENGTH = 2;
 
-	double Bee::WIDTH = 0.8;
+	const double Bee::WIDTH = 0.8;
 
-	int Bee:NUMBER_LIGHT_SENSORS = 4;
+	const double Bee::HEIGHT = 0.4;
 
-	const Position Bee::LIGHT_SENSOR_POSITIONS = {
+	const double Bee::MAX_SPEED = 1;
+
+	const int Bee::NUMBER_LIGHT_SENSORS = 4;
+
+	const double Bee::LIGHT_SENSOR_RANGE = 10;
+
+	const double Bee::LIGHT_SENSOR_WAVELENGTH = 600;
+
+	const Point Bee::LIGHT_SENSOR_POSITIONS[] = {
 		Point (Bee::LENGTH, +Bee::WIDTH * 1.0),
 		Point (Bee::LENGTH, +Bee::WIDTH * 0.8),
 		Point (Bee::LENGTH, -Bee::WIDTH * 0.8),
@@ -41,22 +49,19 @@ namespace Enki
     {
         // Set shape & color
 
-        double len = 2; // Body length
-        double w = 0.8; // Body width
-        double h = 0.4; // Body height
         double m = 1;   // Body mass
 
         //setRectangular(len, w, h, 1);
         Polygone footprint;
-        footprint.push_back(Point(len/2,w/4));
-        footprint.push_back(Point(len/2-w/(4*sqrt(2)),w/2));
-        footprint.push_back(Point(-len/2+w/(4*sqrt(2)),w/2));
-        footprint.push_back(Point(-len/2,w/4));
-        footprint.push_back(Point(-len/2,-w/4));
-        footprint.push_back(Point(-len/2+w/(2*sqrt(2)),-w/2));
-        footprint.push_back(Point(len/2-w/(2*sqrt(2)),-w/2));
-        footprint.push_back(Point(len/2,-w/4));
-        PhysicalObject::Hull hull(PhysicalObject::Part(footprint, h));
+        footprint.push_back (Point (Bee::LENGTH/2,Bee::WIDTH/4));
+        footprint.push_back (Point (Bee::LENGTH/2-Bee::WIDTH/(4*sqrt(2)),Bee::WIDTH/2));
+        footprint.push_back (Point (-Bee::LENGTH/2+Bee::WIDTH/(4*sqrt(2)),Bee::WIDTH/2));
+        footprint.push_back (Point (-Bee::LENGTH/2,Bee::WIDTH/4));
+        footprint.push_back (Point (-Bee::LENGTH/2,-Bee::WIDTH/4));
+        footprint.push_back (Point (-Bee::LENGTH/2+Bee::WIDTH/(2*sqrt(2)),-Bee::WIDTH/2));
+        footprint.push_back (Point (Bee::LENGTH/2-Bee::WIDTH/(2*sqrt(2)),-Bee::WIDTH/2));
+        footprint.push_back (Point (Bee::LENGTH/2,-Bee::WIDTH/4));
+        PhysicalObject::Hull hull(PhysicalObject::Part(footprint, Bee::HEIGHT));
         setCustomHull(hull, m);
         setColor(Color(0.93,0.79,0,1));
 
@@ -67,12 +72,16 @@ namespace Enki
         for (double a = -pi/2; i < 5; i++, a += pi/4) 
         {
             range_sensors[i] = new IRSensor(this, 
-                                            Vector(len/2-sin(a)*w/2, sin(a)*w/2),
+                                            Vector(Bee::LENGTH/2-sin(a)*Bee::WIDTH/2, sin(a)*Bee::WIDTH/2),
                                             0, a, 10, 3731, 0, 0.7, 0);
             addLocalInteraction(range_sensors[i]);
         }
 		  for (i = 0; i < NUMBER_LIGHT_SENSORS; i++) {
-			  this->lightSensor = new LightS
+			  this->lightSensors [i] = new LightSensor
+				  (LIGHT_SENSOR_RANGE, this,
+					LIGHT_SENSOR_POSITIONS [i], 0,
+					LIGHT_SENSOR_WAVELENGTH);
+		  }
     }
 
     /* virtual */
@@ -86,6 +95,87 @@ namespace Enki
 
 	void Bee::controlStep (double dt, const World *world)
 	{
-		
+		double intensity;
+		Vector gradient;
+		senseLight (&intensity, &gradient);
+		bool endLoop = false;
+		for (std::vector<const BehaviourRule *>::iterator ibr =  this->behaviour.begin ();
+			  ibr != this->behaviour.end ();
+			  ibr++) {
+			const BehaviourRule *rule;
+			rule = (*ibr);
+			switch (rule->stimulus) {
+			case LIGHT:
+				if ((rule->usesMinimumValue && (intensity >= rule->minimumValue))
+					 || (rule->usesMaximumValue && (intensity <= rule->maximumValue))) {
+					switch (rule->action) {
+					case MOVE_UP:
+						moveUp (&gradient);
+						break;
+					case MOVE_DOWN:
+						moveDown (&gradient);
+						break;
+					}
+					endLoop = true;
+				}
+				break;
+			case VIBRATION:
+				break;
+			}
+			if (endLoop) {
+				break;
+			}
+		}
+		if (!endLoop) {
+			this->rightSpeed = Bee::MAX_SPEED;
+			this->leftSpeed = Bee::MAX_SPEED * 0.8;
+		}
+ 	}
+
+	void Bee::senseLight (double *intensity, Vector *gradient) const
+	{
+		int i, indexMax, indexMin;
+		indexMax = 0;
+		indexMin = 1;
+		*intensity = 0;
+		for (i = 0; i < NUMBER_LIGHT_SENSORS; i++) {
+			double lsi = this->lightSensors [i]->getIntensity ();
+			if (lsi < this->lightSensors [indexMin]->getIntensity ()) {
+				indexMin = i;
+			}
+			else if (lsi > this->lightSensors [indexMax]->getIntensity ()) {
+				indexMax = i;
+			}
+			*intensity += lsi;
+		}
+		*intensity /= NUMBER_LIGHT_SENSORS;
+		*gradient = LIGHT_SENSOR_POSITIONS [indexMax] - LIGHT_SENSOR_POSITIONS [indexMin];
+	}
+
+
+	void Bee::moveUp (const Vector *gradient)
+	{
+		double delta = normalizeAngle (gradient->angle () - this->angle);
+		if (delta > 0) {
+			this->rightSpeed = Bee::MAX_SPEED;
+			this->leftSpeed = Bee::MAX_SPEED * 0.8;
+		}
+		else {
+			this->rightSpeed = Bee::MAX_SPEED * 0.8;
+			this->leftSpeed = Bee::MAX_SPEED;
+		}
+	}
+
+	void Bee::moveDown (const Vector *gradient)
+	{
+		double delta = normalizeAngle (gradient->angle () - this->angle);
+		if (delta > 0) {
+			this->rightSpeed = Bee::MAX_SPEED * 0.8;
+			this->leftSpeed = Bee::MAX_SPEED;
+		}
+		else {
+			this->rightSpeed = Bee::MAX_SPEED;
+			this->leftSpeed = Bee::MAX_SPEED * 0.8;
+		}
 	}
 }
--- robots/Bee.h
+++ robots/Bee.h
@@ -7,6 +7,8 @@
 #include <enki/robots/DifferentialWheeled.h>
 #include <enki/interactions/IRSensor.h>
 
+#include "../interactions/LightSensor.h"
+#include "../interactions/VibrationSensor.h"
 	
 namespace Enki
 {
@@ -32,6 +34,12 @@ namespace Enki
 		 */
 		static const double WIDTH;
 		/**
+		 * A bee is modelled in Enki by parallelepiped where the width is
+		 * the distance between closed wings.
+		 */
+		static const double HEIGHT;
+		static const double MAX_SPEED;
+		/**
 		 * Number of light sensors in a bee.
 		 */
 		static const int NUMBER_LIGHT_SENSORS;
@@ -39,31 +47,60 @@ namespace Enki
 		 * Light sensor positions relative to bee centre. 
 		 */
 		static const Point LIGHT_SENSOR_POSITIONS [];
+		static const Point LIGHT_SENSOR_ORIENTATIONS [];
+		static const double LIGHT_SENSOR_RANGE;
+		static const double LIGHT_SENSOR_WAVELENGTH;
 		/**
 		 * Actions that a bee can do after sensing some temperature,
-		 * vibration or light value.
+		 * vibration or light value.  The bee senses some gradient, and can
+		 * either move down the gradient or move up the gradient.
 		 */
-		enum Action {APPROACH, MOVE_AWAY, NUMBER_ACTIONS};
+		enum Action {MOVE_DOWN, MOVE_UP, NUMBER_ACTIONS};
 		/**
 		 * Stimulus that a bee reacts to
 		 */
 		enum Stimulus {LIGHT, VIBRATION};
 		/**
-		 * A simple reactive bee behaviour.  A bee may react to some value and change its behaviour.
+		 * A simple reactive bee behaviour.  A bee may react to some value
+		 * and change its behaviour.  Behaviour rules are characterised by a
+		 * sensor range.  The range can have a lower and upper limit.  A rule
+		 * range must have at least one limit otherwise it will not trigger
+		 * an action.
 		 */
-		struct Behaviour
+		struct BehaviourRule
 		{
+			/**
+			 * Minimum value the stimulus should have in order to perform this
+			 * rule action.
+			 */
 			double minimumValue;
+			/**
+			 * Whether this rule range has a lower limit.
+			 */
 			bool usesMinimumValue;
+			/**
+			 * Maximum value the stimulus should have in order to perform this
+			 * rule action.
+			 */
 			double maximumValue;
+			/**
+			 * Whether this rule range has a upper limit.
+			 */
 			bool usesMaximumValue;
-			Action action;
+			/**
+			 * The stimulus assigned to this behaviour rule.
+			 */
 			Stimulus stimulus;
+			/**
+			 * The action to perform.
+			 */
+			Action action;
 		};
+		typedef std::vector<const BehaviourRule *> Behaviour;
 		/**
 		 * Bee behaviour sorted from highest priority to lowest priority.
 		 */
-		const Behaviour *behaviour;
+		 Behaviour behaviour;
 
 	public:
         //! Create a Bee
@@ -75,7 +112,7 @@ namespace Enki
         typedef std::vector<IRSensor*> IRSensorVector;
         IRSensorVector range_sensors;
 
-		typedef std::vector<LightSensor> LightSensorVector;
+		typedef std::vector<LightSensor *> LightSensorVector;
 		/**
 		 * Bee light sensors.
 		 */
@@ -84,10 +121,12 @@ namespace Enki
 		/**
 		 * Update the position of this bee.  
 		 */
-		void controlStep (int dt, const World *);
+		void controlStep (double dt, const World *);
 
 	private:
-		void senseLight () const;
+		void senseLight (double *intensity, Vector *gradient) const;
+		void moveUp (const Vector *gradient);
+		void moveDown (const Vector *gradient);
 	};
 }
 
