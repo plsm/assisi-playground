a457c95f1bedad1c93614ef77cb7a6b6f42261a5
Pedro Mariano <mariano.pedro@gmail.com>
2015-01-06 13:23:51 +0000

    New features in zebra fish.

--- robots/Fish.cpp
+++ robots/Fish.cpp
@@ -4,4 +4,42 @@ using namespace Enki;
 
 double Fish::SCALE_FACTOR = 1.0;
 
+const double Fish::DISTANCE_BETWEEN_WHEELS = 0.8;
+const double Fish::MAX_SPEED = 50.0;
+const double Fish::NOISE_AMOUNT = 0;
+
+const double Fish::COLOUR_RED = 0.6;
+const double Fish::COLOUR_GREEN = 0.6;
+const double Fish::COLOUR_BLUE = 0.6;
+
+const double Fish::COLLISION_ELASTICITY = 0.1;
+const double Fish::DRY_FRICTION_COEFFICIENT = 0.25;
+
+const double Fish::MASS = 10;
+
+const double Fish::WIDTH = 0.8;
+const double Fish::LENGTH = 7;
+const double Fish::HEIGHT = 1.2;
+
 Fish::Fish (double scaleFactor) :
+	DifferentialWheeled (Fish::DISTANCE_BETWEEN_WHEELS, Fish::MAX_SPEED, Fish::NOISE_AMOUNT),
+	color_r_ (Fish::COLOUR_RED),
+	color_g_ (Fish::COLOUR_GREEN),
+	color_b_ (Fish::COLOUR_BLUE)
+{
+	// set shape and mass
+	Polygone footprint;
+	for (double angle = 0; angle < 2 * pi; angle += pi / 3) {
+		double x = Fish::LENGTH / 2 * cos (angle);
+		double y = Fish::WIDTH / 2 * sin (angle);
+		footprint.push_back (Point (x, y) * scaleFactor);
+	}
+	PhysicalObject::Hull hull (PhysicalObject::Part (footprint, Fish::HEIGHT));
+	setCustomHull (hull, Fish::MASS);
+	// set colour
+	this->setColor (Fish::COLOUR_RED, Fish::COLOUR_GREEN, Fish::COLOUR_BLUE);
+	// set other physical properties
+	this->PhysicalObject::collisionElasticity = Fish::COLLISION_ELASTICITY;
+	this->PhysicalObject::dryFrictionCoefficient = Fish::DRY_FRICTION_COEFFICIENT;
+}
+
--- robots/Fish.h
+++ robots/Fish.h
@@ -16,14 +16,69 @@ namespace Enki
 	/**
 	 * A simplistic fish model.
 	 *
+	 * A fish is based on a differential wheeled robot.  
+	 *
+	 * Zebra fish are drawn in a light grey colour.  They are represented by
+	 * an ellipse with length 70mm, width 8mm and height 12mm.
+	 *
 	 * \ingroup robot */
 	class Fish :
 		public DifferentialWheeled,
 		public ExtendedRobot
 	{
 	public:
-		static double SCALE_FACTOR;
-
+		/**
+		 * Default scale factor used when constructing a new fish.
+		 */
+		static const double SCALE_FACTOR;
+		/**
+		 * Distance between the fish 'wheels'.  Unit is centimetre.
+		 */
+		static const double DISTANCE_BETWEEN_WHEELS;
+		/**
+		 * Maximum speed of the zebra fish.
+		 */
+		static const double MAX_SPEED;
+		/**
+		 * Noise used in the motor model.
+		 */
+		static const double NOISE_AMOUNT;
+		/**
+		 * Zebra fish are drawn in a light grey colour.
+		 */
+		static const double COLOUR_RED;
+		/**
+		 * Zebra fish are drawn in a light grey colour.
+		 */
+		static const double COLOUR_GREEN;
+		/**
+		 * Zebra fish are drawn in a light grey colour.
+		 */
+		static const double COLOUR_BLUE;
+		/**
+		 * Zebra fish are soft objects that dissipate 90% of energy.
+		 */
+		static const double COLLISION_ELASTICITY;
+		/**
+		 * The dry friction coefficient mu.
+		 */
+		static const double DRY_FRICTION_COEFFICIENT;
+		/**
+		 * Zebra fish mass in grams.
+		 */
+		static const double MASS = 10;
+		/**
+		 * Zebra fish width.
+		 */
+		static const double WIDTH;
+		/**
+		 * Zebra fish length.
+		 */
+		static const double LENGTH;
+		/**
+		 * Zebra fish height.
+		 */
+		static const double HEIGHT;
 	public:
 		//! Create a Fish
 		Fish (double scaleSize = Fish::SCALE_FACTOR);
